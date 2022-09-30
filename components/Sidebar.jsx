881e53a30cf20ee05635ee66e85f26c5ea2ba00a
lucas_saavedra <lucassaavedra50@gmail.com>
2022-09-30 12:38:44 -0300

    Graphics for bestSellers and bestClients added

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,22 +7,36 @@ const Sidebar = () => {
     return (
         <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
             <div>
-                <p className="text-white text-2xl font-black " >CRM - Clientes</p>
+                <p className="text-white text-2xl font-black " >CRM - Clients</p>
             </div>
             <nav className="mt-5 list-none">
                 <li className={router.pathname === "/" ? "bg-blue-800 p-3" : " p-3"} >
                     <Link href="/" >
-                        <a className="text-white mb-2 block">Clientes</a>
+                        <a className="text-white mb-2 block">Clients</a>
                     </Link>
                 </li>
                 <li className={router.pathname === "/orders" ? "bg-blue-800 p-3" : " p-3"} >
                     <Link href="/orders" >
-                        <a className="text-white mb-2 block">Ordenes</a>
+                        <a className="text-white mb-2 block">Orders</a>
                     </Link>
                 </li>
                 <li className={router.pathname === "/products" ? "bg-blue-800 p-3" : " p-3"} >
                     <Link href="/products" >
                         <a className="text-white mb-2 block">Productos</a>
+                    </Link>
+                </li>
+            </nav>
+            <div className="sm:mt-10">
+                <p className="font-bold text-white text-2xl" >Other Options</p>
+            </div>
+            <nav className="mt-5 list-none">
+                <li className={router.pathname === "/bestsellers" ? "bg-blue-800 p-3" : " p-3"} >
+                    <Link href="/bestsellers" >
+                        <a className="text-white mb-2 block">Best Sellers</a>
+                    </Link>
+                </li><li className={router.pathname === "/bestclients" ? "bg-blue-800 p-3" : " p-3"} >
+                    <Link href="/bestclients" >
+                        <a className="text-white mb-2 block">Best Clients</a>
                     </Link>
                 </li>
             </nav>
